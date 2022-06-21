4a73e413ce8efd9dcdb21965c6c0e71891fb209c
Géssica Sueli Medeiros Marçal <g.sueli@academico.ifrn.edu.br>
2022-06-21 10:34:27 -0300

    aula 4

--- projeto maio 2022/Assets/Scripts/PlayerController.cs
+++ projeto maio 2022/Assets/Scripts/PlayerController.cs
@@ -11,9 +11,18 @@ public class PlayerController : MonoBehaviour
    private Camera _mainCamera;
    private Vector2 _moveInput;
    private Rigidbody _rigidbody;
+
+   private bool _isGrounded;
    
    public float moveMultiplier;
 
+   public float maxVelocity;
+
+   public float rayDistance;
+
+   public LayerMask layerMask;
+
+   public float jumpForce;
    private void OnEnable()
    {
        _rigidbody = GetComponent<Rigidbody>();
@@ -38,15 +47,73 @@ public class PlayerController : MonoBehaviour
        {
            _moveInput = obj.ReadValue<Vector2>();
        }
+
+       if (obj.action.name.CompareTo(_gameControls.Gameplay.Jump.name) == 0)
+       {
+           if(obj.performed) Jump();
+       }
    }
 
    private void Move()
    {
-       _rigidbody.AddForce((_mainCamera.transform.forward * _moveInput.y + _mainCamera.transform.right * _moveInput.x)* moveMultiplier * Time.fixedDeltaTime);
+       Vector3 camForward = _mainCamera.transform.forward; 
+       Vector3 camRight = _mainCamera.transform.right;
+       camForward.y = 0;
+       camRight.x = 0;
+       
+       _rigidbody.AddForce((camForward * _moveInput.y + camRight * _moveInput.x)* moveMultiplier * Time.fixedDeltaTime);
    }
 
    private void FixedUpdate()
    {
       Move();
+      LimitVelocity();
+   }
+
+   private void LimitVelocity()
+   {
+       Vector3 velocity = _rigidbody.velocity;
+
+       if (Mathf.Abs(velocity.x) > maxVelocity) velocity.x = Mathf.Sign(velocity.x) * maxVelocity;
+
+       //velocity.x = Mathf.Clamp(value: velocity.x , min: - maxVelocity, maxVelocity);
+       
+       if (Mathf.Abs(velocity.z) > maxVelocity) velocity.z = Mathf.Sign(velocity.z) * maxVelocity;
+       
+       //velocity.z = Mathf.Clamp(value: velocity.z , min: - maxVelocity, maxVelocity);
+
+       _rigidbody.velocity = velocity; 
+   }
+
+   private void CheckGround()
+   {
+       RaycastHit collision;
+
+       if (Physics.Raycast(transform.position, Vector3.down, out collision, rayDistance, layerMask))
+       {
+           _isGrounded = true;
+       }
+
+       else
+       {
+           _isGrounded = false; 
+       }
+   }
+   private void Jump()
+   {
+       if (_isGrounded)
+       {
+           _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+       }
+   }
+
+   private void Update()
+   {
+       CheckGround();
+   }
+
+   private void OnDrawGizmos()
+   {
+       Debug.DrawRay(start: transform.position, dir: Vector3.down * rayDistance, Color.yellow);
    }
 }
